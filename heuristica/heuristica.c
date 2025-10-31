3e0864e685af381f27defd89d24779ee81ffc370
HELEN KAORI IIJIMA <hki23@inf.ufpr.br>
2025-10-31 16:57:41 -0300

    projeto de heurística começada

---
+++ heuristica/heuristica.c
@@ -0,0 +1,6 @@
+int heuristica (char *tabuleiro) {
+    // cachorros_restantes =
+    // mobilidade_onca =
+    // vitoria = 
+    // return (cachorros_restantes * 3) + (mobilidade_onca * 2) + vitoria;
+}
